fd0e6c3a73e969b464de06129a9516e4243a2711
nholtgrefe <n.a.l.holtgrefe@tudelft.nl>
2025-12-03 09:37:48 +0100

    Implement MixedMultiGraph class and update imports in __init__.py

--- src/phylozoo/core/structure/__init__.py
+++ src/phylozoo/core/structure/__init__.py
@@ -7,7 +7,7 @@ This submodule contains fundamental data structures used throughout the package.
 from ..split import Split, SplitSystem
 from .partition import Partition
 from .circular import CircularOrdering, CircularSetOrdering
-from .mixed_graph import MixedMultiGraph
+from .mixed_multi_graph import MixedMultiGraph
 from .directed_multi_graph import DirectedMultiGraph
 
 __all__ = [
